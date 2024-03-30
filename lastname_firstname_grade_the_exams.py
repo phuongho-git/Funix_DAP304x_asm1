59483eb0a40bf2ca15e2e93a10dfbd956959d177
PhuongHo <phuonghthfx31251@funix.edu.vn>
2024-03-30 23:46:57 +0700

    Task 3

--- lastname_firstname_grade_the_exams.py
+++ lastname_firstname_grade_the_exams.py
@@ -23,52 +23,70 @@ else:
 
 
 
-# đáp án
 
-answer_key = "B,A,D,D,C,B,D,A,C,C,D,B,A,B,A,C,B,D,A,C,A,A,B,D,D"
-right_answer = answer_key.split(",")
 
 # Task 2.1: Báo cáo tổng số dòng dữ liệu được lưu trữ trong tệp.
 
 with open("./Data/class2.txt","r") as readfile:
-   with open("class1_grades.txt","a+") as writefile:
+   with open("task2.txt","a+") as task2:
        exams = readfile.readlines()
        row_count = len(exams)
        print("Total valid lines of data:", row_count)
 
-# Task 2.2: Báo cáo tổng số dòng không hợp lệ và không hợp lệ.
+
+# Task 2.2 và 2.3: Báo cáo tổng số dòng không hợp lệ và không hợp lệ. In các dòng không hợp lệ.
        
-       valid_count = 0
+       valid_lines = []
        for rowno in range(row_count):
            exam = exams[rowno].strip()
-           answer = exam.split(",")
-           id = answer[0]
-
-           # đếm các dòng có 26 giá trị và N# là mục đầu tiên, N# gồm ký tự N đầu tiên và 8 ký tự số phía sau.
-           if (len(answer) == 26 
-               & len(id) == 9 
-               & id[0] == "N" 
-               & id[1:10] == ["0","1", "2", "3","4","5","6","7","8","9"]):
-
-               valid_count += 1
-
-       print("\nTotal valid lines of data:", valid_count)
-       print("\nTotal invalid lines of data:", row_count - valid_count)
-
-# Task 3: In các dòng không hợp lệ ra báo cáo.
-       for rowno in range(row_count):
-           exam = exams[rowno].strip()
-           answer = exam.split(",")
-           id = answer[0]
+           answers = exam.split(",")
+           id = answers[0]
+           
 
            # in các dòng không chứa 26 giá trị
-           if (len(answer) != 26):
-               print("\nInvalid line of data: does not contain exactly 26 values:\n", answer)
+           if (len(answers) != 26):
+               print("\nInvalid line of data: does not contain exactly 26 values:\n", answers)
 
+           # in các dòng có N# không hợp lê
            elif (len(id) != 9 
-               & id[0] != "N" 
-               & id[1:10] != ["0","1", "2", "3","4","5","6","7","8","9"]):
-               print("\nInvalid line of data: N# is invalid\n", answer)      
+               or id[0] != "N"
+               or id[1:10].isnumeric() == False):
+               
+               print("\nInvalid line of data: N# is invalid\n", answers)
+
+           # tạo list chứa các dòng hợp lệ
+           else:              
+               valid_lines = valid_lines + [answers]                
+
+       print("\nTotal valid lines of data:", len(valid_lines))   # in tổng số các dòng hợp lệ
+       print("\nTotal invalid lines of data:", row_count - len(valid_lines))   #in tổng số các dòng không hợp lệ 
+
+
+# Task 3:
+       
+# đáp án
+answer_key = "B,A,D,D,C,B,D,A,C,C,D,B,A,B,A,C,B,D,A,C,A,A,B,D,D"
+right_answer = answer_key.split(",")
+
+# chấm điểm
+with open("./Data/task3.txt","w") as task3:           
+
+    score_list = []
+    for rowno in range(25):
+        
+        if valid_lines[i] == right_answer[i]:
+            score_list.append(4)
+
+        else:
+            score_list.append(-1)
+               
+    print(right_answer[3])
+
+
+
+
+
+      
            
 
 
