b0cc87e5f25cf27dbf7299bedd4db5897ea1bf33
PhuongHo <phuonghthfx31251@funix.edu.vn>
2024-04-20 09:52:41 +0700

    finish task2

--- ho_phuong_grade_the_exams.py
+++ ho_phuong_grade_the_exams.py
@@ -2,6 +2,9 @@
 import os
 import numpy as np
 
+answer_key = "B,A,D,D,C,B,D,A,C,C,D,B,A,B,A,C,B,D,A,C,A,A,B,D,D"
+right_answer = answer_key.split(",")
+
 def checkfile(reportFile):
     if os.path.isfile(reportFile) == 1:
         with open(reportFile, "a+") as writefile:
@@ -19,10 +22,26 @@ def report(classFile,reportFile,name):
             readfile.seek(0)
             exams = [line.rstrip('\n') for line in readfile]
             exam = [line.split(",") for line in exams]
-            for i, answer in enumerate(exam):
+            score_list = []
+            
+            for rowno, answer in enumerate(exam):
                 if (len(answer) != 26):
                     writefile.writelines(
-                        f"\nInvalid line of data: does not contain exactly 26 values:\n{exams[i]}\n")
+                        f"\nInvalid line of data: does not contain exactly 26 values:\n{exams[rowno]}\n")
+                elif (len(answer[0]) != 9) or (answer[0][0] != "N") \
+                    or ((answer[0][1:10]).isnumeric() == False):
+                    writefile.writelines(f"\nInvalid line of data: N# is invalid\n{exams[rowno]}\n")
+                else:
+                    for i in range(25):
+                        if answer[i+1] == right_answer[i]:
+                            answer[i+1] = 4
+                        elif answer[i+1] == "":
+                            answer[i+1] = 0
+                        else:
+                            answer[i+1] = -1
+                    score_list.append(answer)
+            score_array = np.array(score_list,np.newaxis)
+    return score_array     
 
 current_path = os.getcwd()
 datapath = os.path.join(current_path, "Data")
