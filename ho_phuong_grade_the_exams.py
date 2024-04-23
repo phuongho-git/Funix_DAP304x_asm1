26a2b6c764d08bec84ca42f2ab6748c11b1d74b4
PhuongHo <hophuong412@gmail.com>
2024-04-23 16:31:59 +0700

    createDataFrame

--- ho_phuong_grade_the_exams.py
+++ ho_phuong_grade_the_exams.py
@@ -1,5 +1,56 @@
 
+import os
+import numpy as np
+import pandas as pd
 
-           
+# đáp án của bài thi
+answer_key = "B,A,D,D,C,B,D,A,C,C,D,B,A,B,A,C,B,D,A,C,A,A,B,D,D"
+right_answer = answer_key.split(",")
 
+# check file có tồn tại không, nếu có thì viết thông báo RESTART
+def checkfile(reportFile):
+    if os.path.isfile(reportFile) == 1:
+        with open(reportFile, "a+") as writefile:
+            writefile.writelines("\n\n>>> =========================== RESTART " + \
+                                 "===========================\n" + ">>>\n")
 
+# task 2: function xác định các dòng không hợp lệ và tính điểm từng câu trả lời
+def report(classFile,reportFile,name):
+    checkfile(reportFile)
+    with open(classFile, "r") as readfile:
+        with open(reportFile, "a+") as writefile:
+            writefile.writelines(
+                "Enter a class to grade (i.e. class1 for class1.txt): " + name + \
+                    "\nSuccessfully opened " + name + ".txt" + \
+                        "\n\n*** ANALYZING ***\n")
+            readfile.seek(0)
+            exams = [line.rstrip('\n') for line in readfile] # cắt từng dòng
+            exam =  pd.DataFrame([line.split(",") for line in exams])
+            print(exam)
+            
+
+# lấy đường dẫn của folder người dùng
+current_path = os.getcwd()
+# lấy đường dẫn của folder Data chứa file cần phân tích
+datapath = os.path.join(current_path, "Data")
+# nhập tên file từ người dùng
+name = input("Enter a class to grade (i.e. class1 for class1.txt): ")
+filename = name + ".txt"
+# lấy tên file cần phân tích trong folder Data
+filepath = os.path.join(datapath,filename)
+
+# chạy code nếu không có ngoại lệ
+try:
+    # gán biến và chạy function task 2
+    reportFile = "task2.txt"
+    classFile = filepath
+    report(classFile,reportFile,name)
+    
+    
+# catch ngoại lệ khi nhập bàn phím
+except (KeyboardInterrupt, EOFError):
+    print("Keyboard interrupt!")
+
+# catch tất cả các ngoại lệ về sai tên file, đường dẫn, lưu trữ
+except OSError:
+    print("File not found.")
